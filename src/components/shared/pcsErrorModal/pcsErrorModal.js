a06495129d6310f3fe530dc17604f6538393f24b
Stephen Pryor <stpryor@microsoft.com>
2017-10-13 15:16:54 -0700

    Add error modal

--- src/components/shared/pcsErrorModal/pcsErrorModal.js
+++ src/components/shared/pcsErrorModal/pcsErrorModal.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
+import { PcsBtn } from '../pcsBtn/pcsBtn';
 import * as actions from '../../../actions';
 
 import CloseSvg from '../../../assets/icons/Cancel.svg';
@@ -12,17 +13,21 @@ import './pcsModal.css';
 
 class PcsErrorModal extends Component {
   render() {
-    const { actions, svg, children, to } = this.props;
-
     return (
       <div className="pcs-error-modal-container">
         <div className="pcs-modal-close">
-          <span className="pcs-modal-close-btn" onClick={actions.hideModal}>
-            <img src={CloseSvg} alt="Modal icon" />
+          <span className="pcs-modal-close-btn" >
+            <img src={CloseSvg} alt="Modal close icon" />
           </span>
         </div>
-        <div className="pcs-modal-content">{children}</div>
+        <div className="pcs-modal-content">some rando information</div>
+        <div className="pcs-modal-action-btns">
+          <PcsBtn>Close</PcsBtn>
+          <PcsBtn>Close</PcsBtn>
+        </div>
       </div>
     );
   }
 }
+
+
